5c8aa53260811881f1dd695f5fb081b3df2d35e1
violeta <violetagonzalezg@hotmail.com>
2018-07-02 19:36:45 +0200

    Hasta el 41 OK

--- src/components/home/HomePageContainer.jsx
+++ src/components/home/HomePageContainer.jsx
@@ -6,9 +6,18 @@ import { incrementProgress, decrementProgress } from '../../actions/progress';
 import HomePage from './HomePage';
 
 export function HomePageContainer(props) {
+  const { incrementProgressAction, decrementProgressAction } = props;
   return (
     <HomePage
       incrementFunction={incrementProgressAction}
       decrementFunction={decrementProgressAction}
     />  );
 }
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+          incrementProgressAction: incrementProgress,
+          decrementProgressAction: decrementProgress,
+        }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(HomePageContainer);
